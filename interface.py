0e0d1897b34a1ad58915bab3e52ba647a712d7ee
Mendy Dihars <mendy.dihars@laposte.net>
2018-03-18 11:51:05 +0100

    scrapper initialisation

---
+++ interface.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+class Interface:
+
+  def __init__(self):
+    self.run = True
+
+  def scrapping(self, word):
+    url = "https://google.com/search?q=%s" % (word)
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    return soup.find_all('cite')
+
+  def start(self):
+    while (self.run):
+      print("Hello, what are you looking for? (type exit for exit)")
+      word = input('> ')
+      if (word == 'exit'):
+        self.run = False
+      else:
+        for link in self.scrapping(word):
+          print(link.decompose())
+
+app = Interface()
+app.start()
